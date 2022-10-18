03c64aa80fa2c08946c00cbbb2f142b27de0b62b
Aleksandr Obukhov <av_obukhov@rigla.ru>
2022-10-18 09:55:05 +0300

    second commit

--- src/components/BaseTable/BaseTable.vue
+++ src/components/BaseTable/BaseTable.vue
@@ -1,25 +1,18 @@
 <template>
   <div class="panel-wrapper">
-    <b-table
-        striped
-        responsive="sm"
-        :items="tableData"
-        :fields="fieldsTable"
+    <el-table
+        :data="tableData"
+        style="width: 100%"
     >
-      <template #cell(show_details)="row">
-        <b-button size="sm" @click="row.toggleDetails" class="mr-2">
-          {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
-        </b-button>
-      </template>
-      <template #row-details="row">
-        <b-card>
-          <b-row class="mb-2">
-            <b-col>{{ row.item.staff.name }}</b-col>
-            <b-col>{{ row.item.staff.phone }}</b-col>
-          </b-row>
-        </b-card>
-      </template>
-    </b-table>
+      <el-table-column type="expand">
+        <template #default="props">
+          <p m="t-0 b-2">Name: {{ props.row.staff.name }}</p>
+          <p m="t-0 b-2">Phone: {{ props.row.staff.phone }}</p>
+        </template>
+      </el-table-column>
+      <el-table-column label="Name" prop="name" sortable />
+      <el-table-column label="Phone" prop="phone" sortable />
+    </el-table>
   </div>
 </template>
 
